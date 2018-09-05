c7ce15118e500b8ef1b66a613dabb4b6a0171905
Anton <a.nikolsqy@gmail.com>
2018-09-05 23:14:10 +0300

    Eslint fix

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
@@ -12,14 +12,14 @@ var TITLE_TOTAL_HEIGHT = TITLE_Y + TITLE_Y;
 var GRAPH_HEIGHT = 150;
 var GRAPH_WIDTH = 40;
 var GRAPH_GAP = 50;
-var OWN_GRAPH_COLOR = "rgba(255, 0, 0, 1)";
+var OWN_GRAPH_COLOR = 'rgba(255, 0, 0, 1)';
 
-var drawTable = function(context, x, y, color) {
+var drawTable = function (context, x, y, color) {
   context.fillStyle = color;
   context.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxValue = function(timesArray) {
+var getMaxValue = function (timesArray) {
   var maxValue = 0;
   for (var i = 0; i < timesArray.length; i++) {
     if (timesArray[i] > maxValue) {
@@ -29,41 +29,41 @@ var getMaxValue = function(timesArray) {
   return maxValue;
 };
 
-window.renderStatistics = function(ctx, players, times) {
-  drawTable(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, "rgba(0, 0, 0, 0.7)");
-  drawTable(ctx, CLOUD_X, CLOUD_Y, "#fff");
-  ctx.font = "16px PT Mono";
-  ctx.fillStyle = "#000";
-  ctx.fillText("Ура вы победили!", CLOUD_X + GRAPH_GAP, TITLE_Y);
-  ctx.fillText("Список результатов:", CLOUD_X + GRAPH_GAP, TITLE_Y + TITLE_GAP);
+window.renderStatistics = function (ctx, players, times) {
+  drawTable(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  drawTable(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = '#000';
+  ctx.fillText('Ура вы победили!', CLOUD_X + GRAPH_GAP, TITLE_Y);
+  ctx.fillText('Список результатов:', CLOUD_X + GRAPH_GAP, TITLE_Y + TITLE_GAP);
 
   var maxTime = getMaxValue(times);
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillText(
-      players[i],
-      CLOUD_X + GRAPH_GAP + (GRAPH_WIDTH + GRAPH_GAP) * i,
-      CLOUD_HEIGHT - GAP
+        players[i],
+        CLOUD_X + GRAPH_GAP + (GRAPH_WIDTH + GRAPH_GAP) * i,
+        CLOUD_HEIGHT - GAP
     );
     ctx.fillText(
-      Math.round(times[i]),
-      CLOUD_X + GRAPH_GAP + (GRAPH_WIDTH + GRAPH_GAP) * i,
-      CLOUD_HEIGHT - TITLE_Y - GAP - (GRAPH_HEIGHT * times[i]) / maxTime
+        Math.round(times[i]),
+        CLOUD_X + GRAPH_GAP + (GRAPH_WIDTH + GRAPH_GAP) * i,
+        CLOUD_HEIGHT - TITLE_Y - GAP - (GRAPH_HEIGHT * times[i]) / maxTime
     );
   }
   for (var j = 0; j < players.length; j++) {
-    if (players[j] === "Вы") {
+    if (players[j] === 'Вы') {
       ctx.fillStyle = OWN_GRAPH_COLOR;
     } else if (times[j] === maxTime) {
-      ctx.fillStyle = "rgba(0, 0, 204, 1)";
+      ctx.fillStyle = 'rgba(0, 0, 204, 1)';
     } else {
-      ctx.fillStyle = "rgba(0, 0, 204, 0." + Math.round(times[j] / 100) + ")";
+      ctx.fillStyle = 'rgba(0, 0, 204, 0.' + Math.round(times[j] / 100) + ')';
     }
     ctx.fillRect(
-      CLOUD_X + GRAPH_GAP + (GRAPH_WIDTH + GRAPH_GAP) * j,
-      CLOUD_HEIGHT - TITLE_Y - (GRAPH_HEIGHT * times[j]) / maxTime,
-      GRAPH_WIDTH,
-      (GRAPH_HEIGHT * times[j]) / maxTime
+        CLOUD_X + GRAPH_GAP + (GRAPH_WIDTH + GRAPH_GAP) * j,
+        CLOUD_HEIGHT - TITLE_Y - (GRAPH_HEIGHT * times[j]) / maxTime,
+        GRAPH_WIDTH,
+        (GRAPH_HEIGHT * times[j]) / maxTime
     );
   }
 };

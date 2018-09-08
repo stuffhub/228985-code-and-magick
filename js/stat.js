3a9956d5b97e45a36196da2145b21f1be0ea9a6d
Anton <a.nikolsqy@gmail.com>
2018-09-09 01:25:44 +0300

    Внёс правки

--- js/stat.js
+++ js/stat.js
@@ -28,20 +28,27 @@ var getMaxValue = function (timesArray) {
 };
 
 var getGraphColor = function (myScore) {
-  return myScore === 'Вы' ? OWN_GRAPH_COLOR : 'rgba(0, 0, 204, 0.' + Math.random().toFixed(2) + ')';
+  return myScore === 'Вы' ? OWN_GRAPH_COLOR : 'rgba(0, 0, 204, ' + (Math.random().toFixed(2)) + ')';
 };
 
 window.renderStatistics = function (ctx, players, times) {
   drawTable(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   drawTable(ctx, CLOUD_X, CLOUD_Y, '#fff');
   ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
   ctx.fillText('Ура вы победили!', CLOUD_X + GRAPH_GAP, TITLE_Y);
   ctx.fillText('Список результатов:', CLOUD_X + GRAPH_GAP, TITLE_Y + TITLE_GAP);
 
   var maxTime = getMaxValue(times);
 
   for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = getGraphColor(players[i]);
+    ctx.fillRect(
+        CLOUD_X + GRAPH_GAP + (GRAPH_WIDTH + GRAPH_GAP) * i,
+        CLOUD_HEIGHT - TITLE_Y - (GRAPH_HEIGHT * times[i]) / maxTime,
+        GRAPH_WIDTH,
+        (GRAPH_HEIGHT * times[i]) / maxTime
+    );
+    ctx.fillStyle = '#000';
     ctx.fillText(
         players[i],
         CLOUD_X + GRAPH_GAP + (GRAPH_WIDTH + GRAPH_GAP) * i,
@@ -51,15 +58,6 @@ window.renderStatistics = function (ctx, players, times) {
         Math.round(times[i]),
         CLOUD_X + GRAPH_GAP + (GRAPH_WIDTH + GRAPH_GAP) * i,
         CLOUD_HEIGHT - TITLE_Y - GAP - (GRAPH_HEIGHT * times[i]) / maxTime
-    );
-  }
-  for (var j = 0; j < players.length; j++) {
-    ctx.fillStyle = getGraphColor(players[i]);
-    ctx.fillRect(
-        CLOUD_X + GRAPH_GAP + (GRAPH_WIDTH + GRAPH_GAP) * j,
-        CLOUD_HEIGHT - TITLE_Y - (GRAPH_HEIGHT * times[j]) / maxTime,
-        GRAPH_WIDTH,
-        (GRAPH_HEIGHT * times[j]) / maxTime
     );
   }
 };

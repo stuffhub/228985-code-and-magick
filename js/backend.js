c66045c5f2f384dd54e8fdd15716733247105be4
Anton <a.nikolsqy@gmail.com>
2018-10-02 22:43:47 +0300

    Внёс правки

--- js/backend.js
+++ js/backend.js
@@ -1,49 +1,37 @@
 'use strict';
 
 (function () {
+  var createXhr = function (errorMessage, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError(errorMessage, xhr.status);
+      }
+    });
+    return xhr;
+  };
+
   var onErrorDialog = function (statusText, status) {
     var errorDialog = document.createElement('div');
-    errorDialog.style.width = window.constants.ErrorDialog.WIDTH;
-    errorDialog.style.height = window.constants.ErrorDialog.HEIGHT;
-    errorDialog.style.backgroundColor = window.constants.ErrorDialog.BACKGROUND;
-    errorDialog.style.textAlign = window.constants.ErrorDialog.TEXT_ALIGN;
-    errorDialog.style.paddingTop = window.constants.ErrorDialog.PADDING_TOP;
-    errorDialog.style.position = window.constants.ErrorDialog.POSITION;
-    errorDialog.style.top = window.constants.ErrorDialog.TOP;
-    errorDialog.style.left = window.constants.ErrorDialog.LEFT;
-    errorDialog.style.color = window.constants.ErrorDialog.COLOR;
-    errorDialog.style.zIndex = window.constants.ErrorDialog.Z_INDEX;
-    errorDialog.style.marginLeft = window.constants.ErrorDialog.MARGIN_LEFT;
+    errorDialog.classList.add('error-dialog');
     errorDialog.textContent = statusText + ' : ' + status;
     document.body.appendChild(errorDialog);
     setTimeout(function () {
       document.body.removeChild(errorDialog);
     }, 4500);
   };
+
   var load = function (onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Ошибка загрузки данных', xhr.status);
-      }
-    });
+    var xhr = createXhr('Ошибка загрузки данных', onSuccess, onError);
     xhr.open('GET', listCharactersUrl);
     xhr.send();
   };
 
   var save = function (data, onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Ошибка отправки данных', xhr.status);
-      }
-    });
+    var xhr = createXhr('Ошибка отправки данных', onSuccess, onError);
     xhr.open('POST', uploadFormUrl);
     xhr.send(data);
   };
